29e04081930482cc332edb0fa13861f3a830ddca
Krishnaja <kanilba1@asu.edu>
2017-11-09 02:48:35 -0700

    updated governor

--- governor.c
+++ governor.c
@@ -3,9 +3,76 @@
  */
 
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 #include "governor.h"
 
+
+unsigned int get_cpuutil(struct gov_data*)
+{
+//set cpuutil to be a rand value between 0 and 10
+seed = time(NULL);
+srand(seed);   
+int r = rand()%2;
+
+   if (r)
+	{
+	cur_cpuutil = cur_cpuutil + 5;
+		if (cur_cpuutil > 90)
+		cur_cpuutil = 90;
+	}
+   else
+	{
+	cur_cpuutil = cur_cpuutil - 5;
+		if (cur_cpuutil < 10)
+		cur_cpuutil = 10;
+	} 
+
+gov_data->cpuutil = r;
+printf("CPU utilization set to: %d\n", gov_data->cpuutil);
+return r;
+}
+
+unsigned long long find_targfreq(struct gov_data*, unsigned long long curr_freq)
+{
+	//get current cpuutil
+	cur_cpuutil = get_cpuutil(gov_data*);
+
+	//check if current load is below or above threshold
+	if (cur_cpuutil >= DEF_FREQUENCY_UP_THRESHOLD)
+		{
+		printf("CPU utilization crossed upper threshold\n");
+		if (curr_freq == SCALING_MAX_FREQ) //already at maximum scaling freq
+			return curr_freq;
+		else
+		   {
+		    curr_freq = (1 + DEF_FREQUENCY_STEP/100) * curr_freq;
+		    if (curr_freq >= SCALING_MAX_FREQ)
+		    curr_freq = SCALING_MAX_FREQ;
+		    return curr_freq;
+		    }
+	
+		} 
+	else if (cur_cpuutil <= DEF_FREQUENCY_DOWN_THRESHOLD)
+	{
+	printf("CPU utilization below lower threshold\n");
+		if (curr_freq == SCALING_MIN_FREQ) //already at minimum scaling freq
+			return curr_freq;
+		else
+		   {
+		    curr_freq = (1 - DEF_FREQUENCY_STEP/100) * curr_freq;
+		    if (curr_freq <= SCALING_MIN_FREQ)
+		    curr_freq = SCALING_MIN_FREQ;
+		    return curr_freq;
+		    }
+	}
+	printf("Frequency set to: %llu kHz\n", gov_data->cpuutil);
+}
 void gov_init(struct gov_data *gov)
 {
   if(gov==NULL){
@@ -15,6 +82,8 @@ void gov_init(struct gov_data *gov)
 
   gov->cpu_util = 0;
   gov->targ_freq = 0;
+  
+  
 
   return;
 }
--- governor.h
+++ governor.h
@@ -6,13 +6,25 @@
 
 #include "input_params.h"
 
+/* governor macros */
+#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#define DEF_FREQUENCY_STEP			(5)
+#define DEF_SAMPLING_DOWN_FACTOR		(1)
+#define MAX_SAMPLING_DOWN_FACTOR		(10)
+#define SCALING_MAX_FREQ			(2100000)
+#define SCALING_MIN_FREQ			(800000)
+#define RAND_MAX				(10)
+
+unsigned int cur_cpuutil = 10;
+
 struct gov_data{
   unsigned int cpu_util;
   unsigned long long targ_freq;
 };
 
 void gov_init(struct gov_data*);
-void find_targfreq(struct gov_data*, unsigned long long curr_freq);
-void get_cpuutil(struct gov_data*);
+unsigned long long find_targfreq(struct gov_data*, unsigned long long curr_freq);
+unsigned int get_cpuutil(struct gov_data*);
 
 #endif /* GOVERNOR_H */
